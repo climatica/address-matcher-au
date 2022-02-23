8c66947f2830775d44f183f09ec3648fd95595cf
Jade Fountain <jade.fountain@climaterisk.com.au>
2022-02-23 13:53:39 +1100

    structured data load and basic blob

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from ctypes import FormatError
 import sys
 from addressnet.predict import predict
 import psycopg2
+from pathlib import Path
+import pandas as pd
 
 SELECT_STMT = "SELECT gnaf_pid, address, postcode, state, latitude, longitude FROM gnaf_202111.addresses WHERE number_first = %(number_first)s AND street_name = %(street_name)s AND locality_name=%(locality_name)s AND postcode=%(postcode)s AND state=%(state)s LIMIT 1"
 STATES = {
@@ -14,8 +17,26 @@ STATES = {
     "WESTERN AUSTRALIA": "WA",
 }
 
+sep = ", "
+def get_query(d): 
+    return (''
+        + d["FLAT_TYPE_CODE"] + sep
+        + d["FLAT_NUMBER"] + sep
+        + d["BUILDING_NAME"] + sep
+        + d["LEVEL_NUMBER"] + sep
+        + d["LEVEL_TYPE_CODE"] + sep
+        + d["NUMBER_FIRST"] + sep
+        + d["NUMBER_LAST"] + sep
+        + d["LOT_NUMBER"] + sep
+        + d["STREET_NAME"] + sep
+        + d["STREET_TYPE_CODE"] + sep
+        + d["LOCALITY"] + sep
+        + d["STATE_ABBREVIATION"] + sep
+        + d["POSTCODE"]
+    )
 
-def main(filename="test.txt"):
+
+def format_blob_addresses(filename):
     with open(filename) as f:
         a = []
         for line in f:
@@ -29,6 +50,25 @@ def main(filename="test.txt"):
             addrs.append(addr)
             completed += 1
             print(f"Formatted: {completed}/{len(a)} addresses")
+    return addrs
+
+def format_structured_address(filename):
+    ext = Path(filename).suffix 
+    if ext == "csv":
+        data = pd.read_csv(filename)
+    else:
+        raise FormatError(f"Address format not supported: {ext}")
+
+    data["destructured_query"] = get_query(data)
+    print(data)
+    exit()
+
+    
+
+def main(filename="test.txt"):
+
+    addrs = (format_blob_addresses(filename) if Path(filename).suffix == "txt" 
+        else format_structured_address(filename))
 
     conn = psycopg2.connect(
         host="localhost",
@@ -68,6 +108,7 @@ def main(filename="test.txt"):
         cursor.close()
 
     print("Bad addresses: ", bad_addrs)
+
 
 if __name__ == "__main__":
     if(len(sys.argv) < 2):
